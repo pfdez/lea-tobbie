344212308d0cd4d5736b04229260dede91be0bf2
pfdez <31733411+pfdez@users.noreply.github.com>
2022-09-16 15:32:22 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showArrow(ArrowNames.NorthWest)
+basic.pause(5000)
+music.playMelody("F C5 F C5 F C5 F C5 ", 120)
+TobbieII.dance(3)
+basic.showArrow(ArrowNames.NorthEast)
+TobbieII.forward()
+basic.pause(5000)
+TobbieII.stopwalk()
